b75956077d75c401dd25228517bc4c43fad52894
Yvette Lerma <lerma.yvette@hotmail.com>
2024-01-24 12:36:25 -0700

    added all code and slide effect

--- main.js
+++ main.js
@@ -16,7 +16,20 @@ function debounce(func, wait = 20, immediate = true) {
   const sliderImages = document.querySelectorAll('.slide-in');
 
   function checkSlide(e) {
-    console.log(e);
+    sliderImages.forEach(sliderImage => {
+        //half way through the image
+        const slideInAt = (window.scrollY + window.innerHeight) - 
+        sliderImage.height / 2;
+        //this is the bottom of the image
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if(isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active');
+        }
+    })
   }
 
   window.addEventListener('scroll', debounce(checkSlide));
